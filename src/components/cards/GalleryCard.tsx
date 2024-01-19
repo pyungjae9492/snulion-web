0bb63ec7bb4c026c25007466d0d7b3cadfc19a02
pyungjae9492 <ppj050@snu.ac.kr>
2024-01-20 02:22:35 +0900

    Fix

--- src/components/cards/GalleryCard.tsx
+++ src/components/cards/GalleryCard.tsx
@@ -12,7 +12,7 @@ export default function GalleryCard(props: GalleryCardProps) {
   const { imageSrc, type, title, date } = props;
 
   return (
-    <div className='h-[290px] max-w-[350px] overflow-hidden rounded-[10px] bg-white bg-opacity-10'>
+    <div className='h-[275px] max-w-[350px] overflow-hidden rounded-[10px] bg-white bg-opacity-10 md:h-[290px]'>
       <div className='relative h-[210px] w-[350px]'>
         <Image
           src={imageSrc}
@@ -23,7 +23,7 @@ export default function GalleryCard(props: GalleryCardProps) {
           priority
         />
       </div>
-      <div className='flex flex-row items-center justify-between px-5 py-2.5'>
+      <div className='flex flex-row items-center justify-between px-4 py-2.5 md:px-5'>
         <div className='flex flex-col justify-between'>
           <p className='text-[15px] font-semibold md:text-lg'>{title}</p>
           <p className='text-[13px] md:text-base'>{date}</p>
